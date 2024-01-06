6c69a079e235f3d3003147de8f9316d0e09a1fa4
Pratik Khardekar <pratikkhardekar6682@gmail.com>
2024-01-06 22:36:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//   Arithmetic operation by using Numbers 
+
+var firstnumber = 10 ;
+var secondnumber = 20 ;
+
+console.log( firstnumber+secondnumber)
+console.log(firstnumber-secondnumber)
+console.log(firstnumber*secondnumber)
+console.log(firstnumber/secondnumber)
+console.log(firstnumber%secondnumber)
+
+
+// Concat  by using Names 
+
+var firstname = " Ram " ;
+var middlename = "sham" ;
+var lastname = " Dhambere" ;
+
+console.log( firstname+middlename+lastname)
