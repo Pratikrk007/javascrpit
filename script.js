6e17ba22c69dc33f151f0c2e5dd42fe1603d34cf
Pratik Khardekar <pratikkhardekar6682@gmail.com>
2024-01-13 23:59:27 +0530

    Task Update

--- script.js
+++ script.js
@@ -1,44 +1,126 @@
 
 //   Arithmetic operation by using Numbers 
 
-var firstnumber = 10 ;
-var secondnumber = 20 ;
+var firstnumber = 10;
+var secondnumber = 20;
 
-console.log( firstnumber+secondnumber)
-console.log(firstnumber-secondnumber)
-console.log(firstnumber*secondnumber)
-console.log(firstnumber/secondnumber)
-console.log(firstnumber%secondnumber)
+console.log(firstnumber + secondnumber)
+console.log(firstnumber - secondnumber)
+console.log(firstnumber * secondnumber)
+console.log(firstnumber / secondnumber)
+console.log(firstnumber % secondnumber)
 
 
 // Concat  by using Names 
 
-var firstname = " Ram " ;
-var middlename = "sham" ;
-var lastname = " Dhambere" ;
+var firstname = " Ram ";
+var middlename = "sham";
+var lastname = " Dhambere";
 
-console.log( firstname+middlename+lastname)
+console.log(firstname + middlename + lastname)
 
 
 // Array
-var arr = [ 10, 20, "Ram", false ]
-console.log( arr)
+var arr = [10, 20, "Ram", false]
+console.log(arr)
 
 
 // object
 var obj = {
     firstname: "Pratik ",
     middlename: "Rajkumar",
-    surname: " Khardekar" ,
+    surname: " Khardekar",
     age: 26,
 }
-    console.log(obj)
+console.log(obj)
 
-    function myfunction( a, b){
-        console.log(a+b)
-    }
 
-    myfunction( 25, 25 )
+// Use function in js 
+function myfunction(a, b) {
+    console.log(a + b)
+}
+
+myfunction(25, 25)
+
+// By using Assignment Operator  ( = ,+= , -= , *= , /=)
+var name = "pratik";
+var num = 20;
+num = 10;
+console.log(num);
+
+var name = "20";
+var num = 20;
+num += 10;
+console.log(num);
+
+
+var name = "50";
+var num = 20;
+num -= 10;
+console.log(num);
+
+var name = "60";
+var num = 20;
+num *= 10;
+console.log(num);
+
+var name = "10";
+var num = 20;
+num /= 10;
+console.log(num);
+
+// By using Assignment Operator/ Comparision ( == , === , < , > , <= , >= )
+var d =  10;
+var c = 30;
+console.log( d === c);
+
+var d =  20;
+var c = " 20";
+console.log( d == c);
+ 
+var c = 10;
+console.log( c < 10);
+
+var c = 10;
+console.log( c > 10);
+
+var c = 10;
+console.log( c <= 10);
+
+var c = 10;
+console.log( c >= 10);
+
+
+// By using logical Operator (  != , && , ||)
+
+var a = 10 ;
+console.log(a !=" 10");
+
+var b = 20 ;
+ console.log( a == 10 &&  b == 10 );
+
+ var b = 20 ;
+ console.log( a == 10 || b == 10 );
+
+// use Type operator 
+var object = {
+     firstname: "Ram" ,
+     lastname : "sham"
+}
+
+console.log(typeof(object));
+
+var a = 10;
+console.log(typeof(a));
+
+
+
+
+
+
+
+
+
 
 
 
