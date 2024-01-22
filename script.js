6a876394d5979853049de200ebe5abf6fdeb6b12
Pratik Khardekar <pratikkhardekar6682@gmail.com>
2024-01-22 22:53:15 +0530

    Task push

--- script.js
+++ script.js
@@ -1,176 +1,210 @@
 
-//   Arithmetic operation by using Numbers 
+// //   Arithmetic operation by using Numbers 
 
-var firstnumber = 10;
-var secondnumber = 20;
+// var firstnumber = 10;
+// var secondnumber = 20;
 
-console.log(firstnumber + secondnumber)
-console.log(firstnumber - secondnumber)
-console.log(firstnumber * secondnumber)
-console.log(firstnumber / secondnumber)
-console.log(firstnumber % secondnumber)
-
-
-// Concat  by using Names 
-
-var firstname = " Ram ";
-var middlename = "sham";
-var lastname = " Dhambere";
-
-console.log(firstname + middlename + lastname)
+// console.log(firstnumber + secondnumber)
+// console.log(firstnumber - secondnumber)
+// console.log(firstnumber * secondnumber)
+// console.log(firstnumber / secondnumber)
+// console.log(firstnumber % secondnumber)
 
 
-// Array
-var arr = [10, 20, "Ram", false]
-console.log(arr)
+// // Concat  by using Names 
+
+// var firstname = " Ram ";
+// var middlename = "sham";
+// var lastname = " Dhambere";
+
+// console.log(firstname + middlename + lastname)
 
 
-// object
-var obj = {
-    firstname: "Pratik ",
-    middlename: "Rajkumar",
-    surname: " Khardekar",
-    age: 26,
-}
-console.log(obj)
+// // Array
+// var arr = [10, 20, "Ram", false]
+// console.log(arr)
 
 
-// Use function in js 
-function myfunction(a, b) {
-    console.log(a + b)
-}
-
-myfunction(25, 25)
-
-// By using Assignment Operator  ( = ,+= , -= , *= , /=)
-var name = "pratik";
-var num = 20;
-num = 10;
-console.log(num);
-
-var name = "20";
-var num = 20;
-num += 10;
-console.log(num);
+// // object
+// var obj = {
+//     firstname: "Pratik ",
+//     middlename: "Rajkumar",
+//     surname: " Khardekar",
+//     age: 26,
+// }
+// console.log(obj)
 
 
-var name = "50";
-var num = 20;
-num -= 10;
-console.log(num);
+// // Use function in js 
+// function myfunction(a, b) {
+//     console.log(a + b)
+// }
 
-var name = "60";
-var num = 20;
-num *= 10;
-console.log(num);
+// myfunction(25, 25)
 
-var name = "10";
-var num = 20;
-num /= 10;
-console.log(num);
+// // By using Assignment Operator  ( = ,+= , -= , *= , /=)
+// var name = "pratik";
+// var num = 20;
+// num = 10;
+// console.log(num);
 
-// By using Assignment Operator/ Comparision ( == , === , < , > , <= , >= )
-var d = 10;
-var c = 30;
-console.log(d === c);
-
-var d = 20;
-var c = " 20";
-console.log(d == c);
-
-var c = 10;
-console.log(c < 10);
-
-var c = 10;
-console.log(c > 10);
-
-var c = 10;
-console.log(c <= 10);
-
-var c = 10;
-console.log(c >= 10);
+// var name = "20";
+// var num = 20;
+// num += 10;
+// console.log(num);
 
 
-// By using logical Operator (  != , && , ||)
+// var name = "50";
+// var num = 20;
+// num -= 10;
+// console.log(num);
 
-var a = 10;
-console.log(a != " 10");
+// var name = "60";
+// var num = 20;
+// num *= 10;
+// console.log(num);
 
-var b = 20;
-console.log(a == 10 && b == 10);
+// var name = "10";
+// var num = 20;
+// num /= 10;
+// console.log(num);
 
-var b = 20;
-console.log(a == 10 || b == 10);
+// // By using Assignment Operator/ Comparision ( == , === , < , > , <= , >= )
+// var d =  10;
+// var c = 30;
+// console.log( d === c);
 
-// use Type operator 
-var object = {
-    firstname: "Ram",
-    lastname: "sham"
-}
+// var d =  20;
+// var c = " 20";
+// console.log( d == c);
+ 
+// var c = 10;
+// console.log( c < 10);
 
-console.log(typeof (object));
+// var c = 10;
+// console.log( c > 10);
 
-var a = 10;
-console.log(typeof (a));
+// var c = 10;
+// console.log( c <= 10);
 
-// ........
-
-var object1 = {
-    Person1: {
-        firstname: "Ravi",
-        lastname: "shyam ",
-        phone: 6187683618
-    },
-
-    Person2: {
-        firstname: "pratik",
-        lastname: "khardekar ",
-        phone: 82786428
-    },
-
-    Person3: {
-        firstname: "Yash",
-        lastname: "potdar ",
-        phone: 763756799
-    },
-}
-console.log(object1.Person3, object1.Person2, object1.Person1);
+// var c = 10;
+// console.log( c >= 10);
 
 
+// // By using logical Operator (  != , && , ||)
 
-//  ......
-var studentinfo = {
-    firstname: "hgajgd",
-    lastname: " gajdgyua",
-    phonenumbers: [5134654173, 71783878147]
-}
-console.log(studentinfo.phonenumbers);
+// var a = 10 ;
+// console.log(a !=" 10");
+
+// var b = 20 ;
+//  console.log( a == 10 &&  b == 10 );
+
+//  var b = 20 ;
+//  console.log( a == 10 || b == 10 );
+
+// // use Type operator 
+// var object = {
+//      firstname: "Ram" ,
+//      lastname : "sham"
+// }
+
+// console.log(typeof(object));
+
+// var a = 10;
+// console.log(typeof(a));
+
+// // ........
+
+// var object1 = {
+//  Person1 : {
+//     firstname: "Ravi" ,
+//     lastname : "shyam ",
+//     phone : 6187683618  
+//  },
+
+//  Person2 : {
+//     firstname: "pratik" ,
+//     lastname : "khardekar ",
+//     phone : 82786428  
+//  },
+
+//  Person3 : {
+//     firstname: "Yash" ,
+//     lastname : "potdar ",
+//     phone : 763756799  
+//  },
+// }
+//  console.log(object1.Person3,object1.Person2,object1.Person1);
 
 
-//  let/const......
 
-let firstname = " ram ";
-{
-    var firstname = " yash ";
-}
-console.log(firstname);
+//  //  ......
+//  var studentinfo = {
+//  firstname : "hgajgd",
+//  lastname:" gajdgyua",
+//  phonenumbers:[5134654173, 71783878147]
+//  }
+//  console.log(studentinfo.phonenumbers);
+
+
+// //  let/const......
+
+// let firstname = " ram ";
+// {
+//     var firstname =" yash ";
+// }
+// console.log(firstname);
 
 //  using if else 
 
-let age1 = 18;
-let age2 = 17;
+// let age1 = 18 ; 
+// let age2 = 17 ;
 
-if (age1 < age2) {
-    console.log("age2 is not voting age");
+// if ( age1 < age2 ){
+//     console.log( "age2 is not voting age");
+// }else {
+//     console.log( "age1 is voting age");
+// }
+
+
+// creating an array with datatype and loop
+
+var array = [ 10, 20 , 30, 40 ,50 , "ram", "sham", 60, 70,80,90, 100 ];
+console.log(
+    array[10],
+    array[20],
+    array[30],
+    array[40],
+    array[50],
+    array["ram"],
+    array["sham"],
+    array[60],
+    array[70],
+    array[80],
+    array[90],
+    array[100],
+);
+
+for(var  i = 0 ; i < 100; i++ ){
+    console.log(array[i]);
 }
-else {
-    console.log("age1 is voting age");
-}
 
 
 
-
-
+var array = [ 10, 20 , 30, 40 ,50 , "ram", 60, 70, 80, 90, 100 ];
+console.log(
+    array[0],
+    array[1],
+    array[2],
+    array[3],
+    array[4],
+    array[5],
+    array[6],
+    array[7],
+    array[8],
+    array[9],
+    array[10]
+);
 
 
 
